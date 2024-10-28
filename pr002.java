743d115ac7bcdff2cb97c724b04db0e7fb027e33
kolpakova <nastakolpakova2004@gmail.com>
2024-10-28 15:38:55 +0700

    Добавили break и continue

--- pr002.java
+++ pr002.java
@@ -133,6 +133,22 @@ done:
 			System.out.println("После цикла j");
 		}
 		System.out.println("После цикла i");
-	
+		//Оператор continue
+		for(int i = 0; i <= 100; i++){
+			if((i%2) != 0)
+				continue;
+			System.out.print(i + " ");
+		}
+		System.out.println();
+outerloop:
+		for(int i = 1; i < 10; i++){
+			System.out.println("\n Проход внешнего цикла №" + i + ", внутренний цикл: ");
+			for(int j = 1; j < 10; j++){
+				if(j == 5) continue outerloop;
+				System.out.print(j + " ");
+			}
+		}
+		System.out.println();
+		
 	}
 }

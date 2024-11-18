4058a36575643532a0896e27d7d5fc633533928b
kolpakova <nastakolpakova2004@gmail.com>
2024-11-18 17:02:10 +0700

    Демонстрация использования модификаторов доступа

---
+++ Modif.java
@@ -0,0 +1,88 @@
+class AccessDemo{
+	private int alpha; // Переменная с закрытым доступом 
+	public int beta; // открытый доступ
+	int gamma; //Доступ по умолчанию
+	
+	//метод доступа к закрытой переменной
+	public void setAlpha(int a){
+		if(a <= 100)
+			alpha = a;
+		else
+			System.out.println("Значение превышает допустимую границу");
+	}
+	public int getAlpha(){
+		return alpha;
+	}
+	
+}
+
+class FailSoftArray{
+	private int[] a;
+	private int errval;
+	public int length;
+
+	public FailSoftArray(int size, int errv){
+		a = new int[size];
+		errval = errv;
+		length = size;
+	}
+	public int get(int index){
+		if(indexOK(index)){
+			return a[index];
+		}
+		
+		return errval;
+	}
+	public boolean put(int index, int val){
+		if(indexOK(index)){
+			a[index] = val;
+			return true;
+		}
+		return false;
+	}
+	private boolean indexOK(int index){
+		if(index >= 0 & index < length) return true;
+		return false;
+	}
+}
+
+class Modif{
+	public static void main(String[] args){
+		AccessDemo ad = new AccessDemo();
+		//ad.alpha = 99;
+		ad.beta = 1854;
+		ad.gamma = 222;
+
+		ad.setAlpha(199);
+		System.out.println("Значение alpha: " + ad.getAlpha());
+		ad.setAlpha(99);
+		System.out.println("Значение alpha: " + ad.getAlpha());
+
+		System.out.println();
+
+		//Использование "отказоустойчивого" массива
+		FailSoftArray fs = new FailSoftArray(5, -1);
+		int x;
+
+		System.out.println("Вывод значений без сообщений об ошибках");
+		for(int i = 0; i < (fs.length * 2); i++)
+			fs.put(i, i*10);
+		for(int i = 0; i < (fs.length * 2); i++){
+                        x = fs.get(i);
+			if(x != -1) System.out.print(x + " ");
+		}
+
+		System.out.println();
+		
+		System.out.println("Сообщаем об ошибках");
+		for(int i = 0; i < (fs.length * 2); i++)
+                        if(!fs.put(i, i*10))
+				System.out.println("Индекс " + i + " выходит за пределы массива");
+                for(int i = 0; i < (fs.length * 2); i++){
+                        x = fs.get(i);
+                        if(x != -1) System.out.print(x + " ");
+			else System.out.println("Индекс " + i + " выходит за пределы массива");
+                }
+		System.out.println();
+	}
+}
